4d27894ff10a89a3605a9d84495e57110efbca12
Journey <timtag1190@gmail.com>
2025-07-11 00:36:08 -0500

    forgot to update the version number to a dev release.
    Will be making an actual release of `1.3.10` hopefully within the next couple days.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from plex_auto_languages.utils.configuration import Configuration
 from plex_auto_languages.utils.healthcheck import HealthcheckServer
 
 # Version information
-__version__ = "1.3.9"
+__version__ = "1.3.10-dev1"
 
 class PlexAutoLanguages:
     """
